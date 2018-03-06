0cbd2057ff3792df253402c3d3ada75a08ab6a49
David Kennedy <davidken@live.ie>
2018-03-06 21:35:34 +0000

    Sync from Kodi

--- SQLScripts/Music/music.sql
+++ SQLScripts/Music/music.sql
@@ -8,6 +8,8 @@ create table song (
     updated_by varchar(100),
     album_name varchar(256),
     song_name varchar(500),
+    path varchar(500),
+    filename varchar(500),
     play_count integer not null,
     primary key (song_id)
 ) 
@@ -19,25 +21,44 @@ COLLATE utf8_general_ci
     
     CREATE  INDEX song_name_ind ON song (song_name);
     
+    CREATE  INDEX path_ind ON song (path);
     
+    CREATE  INDEX filename_ind ON song (filename);
+    
+
+select * from music.song;
 
 select * from mymusic60.song;
 
 select * from mymusic60.album;
 
-insert into music.song (created_date, created_by, album_name, song_name, play_count) 
-select now(), 'TEMPUSER', a.strAlbum, s.strTitle, 0
-;
+-- New Music
+insert into music.song (created_date, created_by, album_name, song_name, path, filename, play_count) 
+select now(), 'TEMPUSER', a.strAlbum, s.strTitle, p.strPath, s.strFileName, 0
+from mymusic60.song s, mymusic60.album a, mymusic60.path p
+where s.idAlbum = a.idAlbum
+and s.idPath = p.idPath
+and concat(p.strPath, 'z|z', s.strFileName) not in 
+	(select concat(ts.path, 'z|z', ts.filename)
+    from music.song ts);
+    
+-- Deleted Music    
 
+delete from music.song
+where concat(Path, 'z|z', FileName) not in 
+	( select
+    concat(p.strPath, 'z|z', s.strFileName)
+    from mymusic60.song s, mymusic60.album a, mymusic60.path p
+where s.idAlbum = a.idAlbum
+and s.idPath = p.idPath);
 
+-- 20405
 select count(*)
 from mymusic60.song s, mymusic60.album a
 where s.idAlbum = a.idAlbum
-and concat(a.strAlbum, 'z|z', s.strTitle) not in 
-	(select concat(a.strAlbum, 'z|z', s.strTitle)
-    from music.song);
-
-
+union all
+select count(*)
+from music.song;
 
 insert into music.song (album_name, song_name) values ('s', 'd');
 
