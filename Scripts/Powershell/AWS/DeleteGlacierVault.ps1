e37e53c7a1ddbc5691f136b9fa63afd17e016249
David Kennedy <davidken@live.ie>
2020-03-02 19:01:52 +0000

    Tidy up delete archives

--- Scripts/Powershell/AWS/DeleteGlacierVault.ps1
+++ Scripts/Powershell/AWS/DeleteGlacierVault.ps1
@@ -1,40 +1,32 @@
-#
-# DeleteGlacierVault.ps1
-#
+<# .SYNOPSIS
+     Delete Archives from a Glacier Vault
+.DESCRIPTION
+     Submit job to create a list of archives
+	 Delete archives in a loop
+.NOTES     
+#>
+
+$AccountId="138752486395"
+$VaultName="LattuceData2020"
+$Region="eu-west-1"
 
 # Get archives associated with a vault - This kicks off a job...
-aws glacier initiate-job --job-parameters "{""Type"": ""inventory-retrieval""}" --account-id ACCOUNT_ID --region REGION --vault-name VAULT_NAME
+aws glacier initiate-job --job-parameters "{""Type"": ""inventory-retrieval""}" --account-id $AccountId --region $Region --vault-name $VaultName
 
 # Status of job
-aws glacier list-jobs --account-id ACCOUNT_ID --region REGION --vault-name VAULT_NAME 
+aws glacier list-jobs --account-id $AccountId --region $Region --vault-name $VaultName 
 
 # Output Job to a text file - this file contains the archives in a vault
-aws glacier get-job-output --account-id ACCOUNT_ID --region REGION --vault-name VAULT_NAME --job-id FROM_PREV_COMMAND ./OutputFile.json
+aws glacier get-job-output --account-id $AccountId --region $Region --vault-name $VaultName --job-id FROM_PREV_COMMAND ./OutputFile.json
 
-
-
-# Run through the vault and delete archives in turn
-# helper to turn PSCustomObject into a list of key/value pairs
-function Get-ObjectMembers {
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
-        [PSCustomObject]$obj
-    )
-    $obj | Get-Member -MemberType NoteProperty | ForEach-Object {
-        $key = $_.Name
-        [PSCustomObject]@{Key = $key; Value = $obj."$key"}
-    }
-}
-
-$json = Get-Content -Raw .\output_LattuceBackup.json
+$json = Get-Content -Raw .\OutputFile.json
 $Glacier=$json | ConvertFrom-Json
 $ArchiveList=$Glacier.ArchiveList
 
 foreach ($ArchiveId in $ArchiveList.ArchiveId) {
 	$Count++
 	write-output "Deleting Archive ID: $($ArchiveId) ($($Count) of $($ArchiveList.ArchiveId.Count))"
-	aws glacier delete-archive --archive-id=$($ArchiveId) --vault-name VAULT_NAME --account-id ACCOUNT_ID --region REGION
+	aws glacier delete-archive --archive-id=$($ArchiveId) --vault-name $VaultName --account-id $($AccountId) --region $Region
 }
 
 		
